16053a40edb157a4e595fb3261e411b2408df44a
James Rhodes <james21198@gmail.com>
2024-03-16 11:09:21 +0000

    questions js file updated

---
+++ utils/questions.js
@@ -0,0 +1,34 @@
+const questions = [
+    {
+        type: 'input',
+        name: 'descirption',
+        question: 'how would you describe your project?',
+    },
+    {
+        type: 'input',
+        name: 'problem',
+        question: 'What have you learnt?',
+    },
+    {
+        type: 'input',
+        name: 'project',
+        question: 'Why did you build this project?',
+    },
+    {
+        type: 'input',
+        name: 'problem',
+        question: 'What problem does this solve?',
+    },
+    {
+        type: 'input',
+        name: 'problem',
+        question: 'What have you learnt?',
+    },
+    {
+        type: 'input',
+        name: 'stand',
+        question: 'What makes your project stand out?',
+    },
+]
+
+module.exports = questions;
