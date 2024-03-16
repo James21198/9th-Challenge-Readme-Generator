6f1a5126fe050b9e58fe31152787dbfdd5d4c2a2
James Rhodes <james21198@gmail.com>
2024-03-16 11:14:13 +0000

    index js file updated

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const fs = require('fs');
 
 const generateMarkdown = require('./utils/generateMarkdown');
 const questions = require('./utils/questions');
+
+function createFile(markdown) {
+    fs.writeFile('readme.md', markdown, (err) => 
+        err ? console.log(err) : console.log('Readme file successfully created!')
+    );
+}
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            const markdown = generateMarkdown(answers);
+            createFile(markdown);
+        });
+}
+
+init();
