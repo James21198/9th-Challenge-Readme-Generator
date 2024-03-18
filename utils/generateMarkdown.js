673e6c3a6ab874273daab9a2de90e5b9076195b8
James Rhodes <james21198@gmail.com>
2024-03-18 18:29:07 +0000

    License Badges Added

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,26 @@
-function renderLicenseBadge(data) {
-  let licenseType = data.license;
-  if (licenseType === 'MIT') {
-    data.license = `[License: MIT](https://img.shields.io/badge/license-MIT-blue)`
+function renderLicenseBadge(license) {
+  let licenseBadge = ""
+
+  switch(license) {
+    case 'MIT': 
+      licenseBadge = `![Static Badge](https://img.shields.io/badge/license-MIT-blue)`
+      break;
+     case 'APACHE 2.0': 
+      licenseBadge = `![Static Badge](https://img.shields.io/badge/license/apache-2.0-blue)`
+      break;
+    case 'GPL 3.0':
+      licenseBadge = `![Static Badge](https://img.shields.io/badge/license-GPLv3-blue)`
+      break;
+    case 'BSD 3':
+      licenseBadge = `![Static Badge](https://img.shields.io/badge/BSD-3-Purple)`
+      break;
+    case 'None':
+      licenseBadge = `No License Used`
+      break;
   }
+  return licenseBadge
 }
+
 
 function markdown(data) {
   return `# ${data.title}
--- utils/questions.js
+++ utils/questions.js
@@ -2,17 +2,17 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        question: 'What is the title of your project?',
+        message: 'What is the title of your project?',
     },
     {
         type: 'input',
         name: 'description',
-        question: 'how would you describe your project?',
+        message: 'how would you describe your project?',
     },
     {
         type: 'input',
         name: 'installation',
-        question: 'What do you need to do to install this project?',
+        message: 'What do you need to do to install this project?',
     },
     {
         type: 'list',
@@ -22,28 +22,28 @@ const questions = [
     {
         type: 'input',
         name: 'test',
-        question: 'How do you test your project?',
+        message: 'How do you test your project?',
         default: 'npm test',
     },
     {
         type: 'input',
         name: 'usage',
-        question: 'How do you use your project?',
+        message: 'How do you use your project?',
     },
     {
         type: 'input',
         name: 'contribution',
-        question: 'Has any other users contributed to this project, if so please list them',
+        message: 'Has any other users contributed to this project, if so please list them',
     },
     {
         type: 'input',
         name: 'github',
-        question: 'What is your github username?',
+        message: 'What is your github username?',
     },
     {
         type: 'input',
         name: 'email',
-        question: 'What is your email address?',
+        message: 'What is your email address?',
     },
 ]
 
